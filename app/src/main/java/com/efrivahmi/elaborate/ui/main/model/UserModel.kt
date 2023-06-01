14ae6535697ce8ac098450fbee2f1312ea32c55f
Xavier Mexious <vinsensiusadrian123@gmail.com>
2023-06-01 18:38:30 +0700

    feat: model user, preference
    menambahkan user model dan user preference

---
+++ app/src/main/java/com/efrivahmi/elaborate/ui/main/model/UserModel.kt
@@ -0,0 +1,12 @@
+package com.efrivahmi.elaborate.ui.main.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class UserModel(
+    val name: String,
+    val email: String,
+    val password: String,
+    val isLogin: Boolean
+): Parcelable
