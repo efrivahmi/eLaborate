a42efca46430e37362e91dd1a42bff112565aece
Xavier Mexious <vinsensiusadrian123@gmail.com>
2023-06-15 01:02:56 +0700

    feat: Under Development
    menambahkan under development activity

--- app/src/main/java/com/efrivahmi/elaborate/ui/main/home/HomeFragment.kt
+++ app/src/main/java/com/efrivahmi/elaborate/ui/main/home/HomeFragment.kt
@@ -14,6 +14,7 @@ import com.efrivahmi.elaborate.ui.main.MainViewModel
 import com.efrivahmi.elaborate.ui.main.diagnose.DiagnoseActivity
 import com.efrivahmi.elaborate.ui.main.diagnose.DiagnoseViewModel
 import com.efrivahmi.elaborate.ui.main.result.ResultActivity
+import com.efrivahmi.elaborate.ui.underdevelop.UnderDevelopmentActivity
 import com.efrivahmi.elaborate.utils.ViewModelFactory
 import com.efrivahmi.elaborate.utils.ViewModelFactoryMl
 
@@ -31,7 +32,37 @@ class HomeFragment : Fragment() {
 
         activityDiagnose()
         activityResult()
+        activityMedicine()
+        activityWorkout()
+        activityDoctor()
         setupUserViewModel()
+    }
+
+    private fun activityDoctor() {
+        binding.ivDoctor.setOnClickListener {
+            activity?.let {
+                val intent = Intent(it, UnderDevelopmentActivity::class.java)
+                it.startActivity(intent)
+            }
+        }
+    }
+
+    private fun activityWorkout() {
+        binding.ivWorkout.setOnClickListener {
+            activity?.let {
+                val intent = Intent(it, UnderDevelopmentActivity::class.java)
+                it.startActivity(intent)
+            }
+        }
+    }
+
+    private fun activityMedicine() {
+        binding.ivMedicine.setOnClickListener {
+            activity?.let {
+                val intent = Intent(it, UnderDevelopmentActivity::class.java)
+                it.startActivity(intent)
+            }
+        }
     }
 
     private fun setupUserViewModel() {
---
+++ app/src/main/java/com/efrivahmi/elaborate/ui/underdevelop/UnderDevelopmentActivity.kt
@@ -0,0 +1,57 @@
+package com.efrivahmi.elaborate.ui.underdevelop
+
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.efrivahmi.elaborate.R
+import com.efrivahmi.elaborate.databinding.ActivityUnderDevelopmentBinding
+
+class UnderDevelopmentActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityUnderDevelopmentBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityUnderDevelopmentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupView()
+        playAnimation()
+    }
+
+    private fun playAnimation() {
+        ObjectAnimator.ofFloat(binding.tvLogoElaborate, View.TRANSLATION_Y, -45f, 45f).apply {
+            duration = 2000
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+        }.start()
+
+        val under1 = ObjectAnimator.ofFloat(binding.tvUnderDevelopment, View.ALPHA, 1f).setDuration(2000)
+        val under2 = ObjectAnimator.ofFloat(binding.tvUnderDevelopment2, View.ALPHA, 1f).setDuration(2000)
+
+        val together = AnimatorSet().apply {
+            playTogether(under1, under2)
+        }
+
+        AnimatorSet().apply {
+            playSequentially(together)
+            start()
+        }
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+}
