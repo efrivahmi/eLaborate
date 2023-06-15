4e4a7955608a9ab22d0b99f0d2dcd02c26d501d8
Xavier Mexious <vinsensiusadrian123@gmail.com>
2023-06-16 03:18:06 +0700

    comment: comment result view model

--- app/src/main/java/com/efrivahmi/elaborate/ui/main/result/ResultViewModel.kt
+++ app/src/main/java/com/efrivahmi/elaborate/ui/main/result/ResultViewModel.kt
@@ -1,7 +1,12 @@
 package com.efrivahmi.elaborate.ui.main.result
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.efrivahmi.elaborate.data.api.ml.DataSourceDiagnose
+import com.efrivahmi.elaborate.data.response.DResponse
+import com.efrivahmi.elaborate.data.response.DiagnoseResponse
 
-class ResultViewModel(private val dataSourceMl: DataSourceDiagnose) : ViewModel() {
-}
+//class ResultViewModel(private val dataSourceMl: DataSourceDiagnose) : ViewModel() {
+//    val isLoading: LiveData<Boolean> = dataSourceMl.isLoading
+//    val diagnoseResult: LiveData<DiagnoseResponse> = dataSourceMl.diagnose
+//}
